b040c4a128f31077b22a19942d0812973ece795e
iamsafin <ahmedshafinshafi@gmail.com>
2024-01-28 21:43:36 +0600

    module 2.3 and 2.4 added

---
+++ Module-2.4/Program.cs
@@ -0,0 +1,9 @@
+﻿Random random = new ();
+
+int roll1 = random.Next(1, 7);
+int roll2 = random.Next(100);
+int roll3 = random.Next(500);
+
+Console.WriteLine($"First roll is {roll1}");
+Console.WriteLine($"Second roll is {roll2}");
+Console.WriteLine($"Third roll is {roll3}");
