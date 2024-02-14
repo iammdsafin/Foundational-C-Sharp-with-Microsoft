f2ca915d5e0720abf713cb89e3dad4ad7a020a8c
iamsafin <ahmedshafinshafi@gmail.com>
2024-02-14 19:14:08 +0600

    module 3.2 added

---
+++ Module-3.2/Program.cs
@@ -0,0 +1,76 @@
+﻿//bool flag = true;
+////bool flag = false;
+//int value1 = 0;
+
+//if (flag)
+//{
+//    int value = 10;
+//    Console.WriteLine($"The value inside the code block is {value}");
+//}
+
+//value1 = 20;
+//Console.WriteLine($"The value outside the code block is {value1}");
+
+//int firstVariable = 5;
+
+//if (firstVariable > 0)
+//{
+//    int secondVariable = 6;
+//    firstVariable = firstVariable + secondVariable;
+//}
+//Console.WriteLine(firstVariable);
+
+//bool var = true;
+//if (var)
+//    Console.WriteLine(var);
+
+//bool var2 = true;
+//if (var2) Console.WriteLine(var2);
+
+//string name = "404 Requested User Not Found";
+
+//if (name == "Ahmed Shafin") 
+//    Console.WriteLine("Ahmed Shafin found");
+//else if (name == "404 Requested User Not Found") 
+//    Console.WriteLine("404 Requested User found");
+//else 
+//    Console.WriteLine("No User Found");
+
+//int[] numbers = { 4, 8, 15, 16, 23, 42 };
+//int total = 0;
+//bool found = false;
+
+//foreach (int number in numbers)
+//{     
+//    total += number;
+
+//    if (number == 42)
+//    {
+//        found = true;
+//    }
+
+//    if (found)
+//    {
+//        Console.WriteLine("Set contains 42");
+//    }
+//    Console.WriteLine($"Total: {total}");
+//}
+
+//int firstVariable = 1;
+//if (firstVariable > 0)
+//{
+//    int secondVariable = 8;
+//    firstVariable = firstVariable + secondVariable;
+//}
+//Console.WriteLine(firstVariable);
+
+//int firstVariable = 9;
+//if (firstVariable > 0)
+//{
+//    int secondVariable = 6;
+//}
+//secondVariable = firstVariable;
+//Console.WriteLine(firstVariable);
+
+
+
