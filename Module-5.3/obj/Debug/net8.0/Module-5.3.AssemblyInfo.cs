95dc035f96f218f6a0f3743431778bfb8031e5e0
iamsafin <ahmedshafinshafi@gmail.com>
2024-03-11 19:23:12 +0600

    5.3 modules mini dice game added

--- Module-5.3/obj/Debug/net8.0/Module-5.3.AssemblyInfo.cs
+++ Module-5.3/obj/Debug/net8.0/Module-5.3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module-5.3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c004a8d04d9b64cf96a20886690614e26e6a9fb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c9a386bbe25a047db51094aea35704de061d98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module-5.3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module-5.3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
