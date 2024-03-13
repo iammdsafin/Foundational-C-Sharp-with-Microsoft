8a6966fdcdd7e4ac71b35887adb80c41ecd01461
iamsafin <ahmedshafinshafi@gmail.com>
2024-03-13 16:00:27 +0600

    exam code added

---
+++ Exam/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Program
+{
+    public static int MultiplyNumbers(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+    public static void Main(string[] args)
+    {
+        int result = MultiplyNumbers(3, 4);
+        Console.WriteLine("Result: " + result);
+    }
+}
+
+
+
+int x = 5;
+int y = 2;
+int result = x / y;
+Console.WriteLine(result);
+
+
+int y = 5;
+void PrintValue()
+{
+    int y = 10;
+    Console.WriteLine(y);
+}
+PrintValue();
+
+
+void DisplayFullName(string firstName, string lastName)
+{
+    Console.WriteLine("Full Name: " + firstName + " " + lastName);
+}
+DisplayFullName("Ahmed", "Shafin");
+
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+foreach (int num in numbers)
+{
+    Console.WriteLine(num);
+}
